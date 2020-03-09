dfdfb9e66339544c719bbbea0c04e4d1488af7b2
lookrecius <bragin.boris@ya.ru>
2020-03-10 00:40:31 +0300

    Add high-level functions into movie.find()

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+let movies = [
+    { id: '1', name: 'Pulp Fiction', genre: 'Crime'},
+    { id: '2', name: 'Odyssey 2001', genre: 'Sci-Fi'},
+    { id: 3, name: 'Matrix', genre: 'Sci-Fi-Thriller'},
+    { id: 4, name: 'Gentlemen', genre: 'Crime-Comedy'},
+];
 
 const MovieType = new  GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
     }),
@@ -17,9 +24,9 @@ const Query = new GraphQLObjectType({
             type: MovieType,
             args: { id: { type: GraphQLString} },
             resolve(parent, args) {
-
+                return movies.find(movie => (movie.id === args.id));
             }
-        },C
+        },
     }
 
 });
