04bc7a381e5f6534821b46887f17c6e599c36c28
lookrecius <bragin.boris@ya.ru>
2020-03-09 23:13:38 +0300

    Create Root Query (const Query) and export to schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const MovieType = new  GraphQLObjectType({
     name: 'Movie',
@@ -8,4 +8,21 @@ const MovieType = new  GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
     }),
-})
+});
+
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        movie: {
+            type: MovieType,
+            args: { id: { type: GraphQLString} },
+            resolve(parent, args) {
+
+            }
+        },
+    }
+
+});
+    module.exports = new GraphQLSchema({
+        query: Query,
+    });
