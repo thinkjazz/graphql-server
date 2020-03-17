f61c1bf9bc84b2c7ee273ba266e086ba3bab5c46
lookrecius <bragin.boris@ya.ru>
2020-03-18 02:44:42 +0300

    Modify root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,15 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 let movies = [
     { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
     { id: '2', name: 'Odyssey 2001', genre: 'Sci-Fi', directorId: '2', },
     { id: '3', name: 'Matrix', genre: 'Sci-Fi-Thriller', directorId: '3', },
     { id: '4', name: 'Gentlemen', genre: 'Crime-Comedy', directorId: '4', },
+    { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1', },
+    { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1', },
+    { id: '7', name: 'Django', genre: 'Crime', directorId: '1', },
+    { id: '8', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
 ];
 
 let directors = [
@@ -36,6 +40,12 @@ const DirectorType = new  GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent,args) {
+                return movies.filter((movie) => movie.directorId === parent.id);
+            }
+        }
     }),
 });
 
@@ -54,11 +64,23 @@ const Query = new GraphQLObjectType({
             args: { id: { type: GraphQLID} },
             resolve(parent, args) {
                 return directors.find((director) => (director.id === args.id));
+            },
+        },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent, args) {
+                return movies;
             }
         },
+        directors: {
+            type: new GraphQLList(DirectorType),
+            resolve(parent, args) {
+                return directors;
+            }
+        }
     }
-
 });
+
     module.exports = new GraphQLSchema({
         query: Query,
     });
